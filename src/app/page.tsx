b00a3de38b66c7a7941b7f5775d152a7322e39a2
Abdul Rehman <91366959+rehmanNRY@users.noreply.github.com>
2024-11-12 00:22:44 +0500

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import AuthScreen from "@/features/auth/components/auth-screen";
 export default function Home() {
   return (
     <div className="bg-[#5c3b58] h-full w-full">
-
       <AuthScreen />
     </div>
   );
