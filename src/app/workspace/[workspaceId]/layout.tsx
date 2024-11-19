2f831292c294c57479e0172cbbe79b4127a0a025
Abdul Rehman <91366959+rehmanNRY@users.noreply.github.com>
2024-11-19 23:18:56 +0500

    workspace header

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -2,6 +2,14 @@
 import Sidebar from "./Sidebar";
 import Toolbar from "./toolbar";
 
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable"
+import WorkspaceSidebar from "./workspace-sidebar";
+
+
 interface WorkspaceIdLayoutProps {
   children: React.ReactNode;
 }
@@ -12,7 +20,22 @@ const WorkspaceIdLayout = ({children}: WorkspaceIdLayoutProps) => {
       <Toolbar />
       <div className="flex h-[calc(100vh-40px)]">
         <Sidebar />
-        {children}
+        <ResizablePanelGroup
+          direction="horizontal"
+          autoSaveId={"abdul-workspace-layout"}
+        >
+          <ResizablePanel
+            defaultSize={20}
+            minSize={11}
+            className="bg-[#5e2c5f]"
+          >
+            <WorkspaceSidebar />
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel minSize={20} >
+            {children}
+          </ResizablePanel>
+        </ResizablePanelGroup>
       </div>
     </div>
   )
