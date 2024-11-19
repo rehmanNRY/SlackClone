a89b2a4e2ccdcd26d1fd5f95619bfb6b5146e5a7
Abdul Rehman <91366959+rehmanNRY@users.noreply.github.com>
2024-11-19 16:46:50 +0500

    sidebar component

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,14 +1,7 @@
-"use client"
-
-import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
-import { useWorkspaceId } from "@/hooks/use-workspace-id";
-
 const WorkspaceIdPage = () => {
-  const workspaceId = useWorkspaceId();
-  const {data} = useGetWorkspace({id: workspaceId})
   return (
     <div>
-      Data: {JSON.stringify(data)}
+       Workspace id page
     </div>
   )
 }
