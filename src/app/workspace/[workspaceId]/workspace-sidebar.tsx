64916312a04d851cb2e7dafdee1c75bd4b3c439d
Abdul Rehman <91366959+rehmanNRY@users.noreply.github.com>
2024-12-18 23:12:25 +0500

    channels & members

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -1,15 +1,26 @@
 import { useCurrentMember } from '@/features/members/api/use-current-member';
 import { useGetWorkspace } from '@/features/workspaces/api/use-get-workspace';
 import { useWorkspaceId } from '@/hooks/use-workspace-id'
-import { AlertTriangle, Loader } from 'lucide-react';
+import { AlertTriangle, HashIcon, Loader, MessageSquareText, SendHorizonal, Sidebar } from 'lucide-react';
 import React from 'react'
 import WorkspaceHeader from './workspace-header';
+import { SidebarItem } from './sidebar-item';
+import { useGetChannels } from '@/features/channels/api/use-get-channels';
+import { WorkspaceSection } from './workspace-section';
+import { useGetMembers } from '@/features/members/api/use-get-members';
+import { UserItem } from './user-item';
 
 const WorkspaceSidebar = () => {
   const workspaceId = useWorkspaceId();
 
   const { data: member, isLoading: memberLoading } = useCurrentMember({ workspaceId });
   const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({ id: workspaceId });
+
+  const { data: channels, isLoading: channelsLoading } = useGetChannels({ workspaceId });
+
+  const { data: members, isLoading: membersLoading } = useGetMembers({ workspaceId })
+
+
 
   if (memberLoading || workspaceLoading) {
     return (
@@ -33,6 +44,46 @@ const WorkspaceSidebar = () => {
   return (
     <div className='flex flex-col bg-[#5e2c5f] h-full'>
       <WorkspaceHeader workspace={workspace} isAdmin={member.role === "admin"} />
+      <div className="flex flex-col">
+        <SidebarItem
+          label='Threads'
+          icon={MessageSquareText}
+          id="threads"
+        />
+        <SidebarItem
+          label='Drafts & Sent'
+          icon={SendHorizonal}
+          id="drafts"
+        />
+      </div>
+      <WorkspaceSection
+        label="Channels"
+        hint="New channel"
+        onNew={() => { }}
+      >
+        {channels?.map((item) => (
+          <SidebarItem
+            key={item._id}
+            icon={HashIcon}
+            label={item.name}
+            id={item._id}
+          />
+        ))}
+      </WorkspaceSection>
+      <WorkspaceSection
+        label="Direct Messages"
+        hint="New direct message"
+        onNew={() => { }}
+      >
+        {members?.map((item) => (
+          <UserItem
+            key={item._id}
+            id={item._id}
+            label={item.user.name}
+            image={item.user.image}
+          />
+        ))}
+      </WorkspaceSection>
     </div>
   )
 }
--- src/features/auth/components/user-button.tsx
+++ src/features/auth/components/user-button.tsx
@@ -29,9 +29,9 @@ const UserButton = () => {
   return (
     <DropdownMenu modal={false} >
       <DropdownMenuTrigger className='outline-none relative'>
-        <Avatar className='size-10 hover:opacity-75 transition'>
-          <AvatarImage alt={name} src={image} />
-          <AvatarFallback className="bg-pink-600 text-white">
+        <Avatar className='rounded-md size-10 hover:opacity-75 transition'>
+          <AvatarImage className='rounded-md' alt={name} src={image} />
+          <AvatarFallback className="rounded-md bg-sky-500 text-white">
             {avatarFallback}
           </AvatarFallback>
         </Avatar>
