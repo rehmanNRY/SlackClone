0d567584ad2a1c541be80e2003330b61263526e8
Abdul Rehman <91366959+rehmanNRY@users.noreply.github.com>
2024-12-19 23:36:28 +0500

    create channel functionality

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -9,9 +9,12 @@ import { useGetChannels } from '@/features/channels/api/use-get-channels';
 import { WorkspaceSection } from './workspace-section';
 import { useGetMembers } from '@/features/members/api/use-get-members';
 import { UserItem } from './user-item';
+import { useCreateChannelModal } from '@/features/channels/store/use-create-channel-modal';
 
 const WorkspaceSidebar = () => {
   const workspaceId = useWorkspaceId();
+
+  const [_open, setOpen] = useCreateChannelModal();
 
   const { data: member, isLoading: memberLoading } = useCurrentMember({ workspaceId });
   const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({ id: workspaceId });
@@ -59,7 +62,7 @@ const WorkspaceSidebar = () => {
       <WorkspaceSection
         label="Channels"
         hint="New channel"
-        onNew={() => { }}
+        onNew={ member.role === "admin" ? () => setOpen(true) : undefined }
       >
         {channels?.map((item) => (
           <SidebarItem
---
+++ src/features/channels/store/use-create-channel-modal.ts
@@ -0,0 +1,9 @@
+import {atom, useAtom} from "jotai";
+
+const modalState = atom(false);
+
+export const useCreateChannelModal = () => {
+
+  return useAtom(modalState);
+}
+
