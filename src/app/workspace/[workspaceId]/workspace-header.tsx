be2eeb0a8470383f8a87537b319384fb2181a08d
Abdul Rehman <91366959+rehmanNRY@users.noreply.github.com>
2024-12-20 20:52:29 +0500

    workspace invite modal

--- src/app/workspace/[workspaceId]/workspace-header.tsx
+++ src/app/workspace/[workspaceId]/workspace-header.tsx
@@ -12,6 +12,7 @@ import { ChevronDown, ListFilter, SquarePen } from "lucide-react";
 import Hint from "@/components/hint";
 import PreferencesModal from "./preferences-modal";
 import { useState } from "react";
+import { InviteModal } from "./invite-modal";
 
 interface WorkspaceHeaderProps {
   workspace: Doc<"workspaces">;
@@ -20,8 +21,15 @@ interface WorkspaceHeaderProps {
 
 const WorkspaceHeader = ({ workspace, isAdmin }: WorkspaceHeaderProps) => {
   const [preferencesOpen, setPreferencesOpen] = useState(false)
+  const [inviteOpen, setInviteOpen] = useState(false)
   return (
     <>
+    <InviteModal 
+      open={inviteOpen}
+      setOpen={setInviteOpen}
+      name={workspace.name}
+      joinCode={workspace.joinCode}
+    />
     <PreferencesModal open={preferencesOpen} setOpen={setPreferencesOpen} initialValue={workspace.name} />
     <div className='flex items-center justify-between px-4 h-[49px] gap-0.5'>
       <DropdownMenu>
@@ -54,7 +62,7 @@ const WorkspaceHeader = ({ workspace, isAdmin }: WorkspaceHeaderProps) => {
               <DropdownMenuSeparator />
               <DropdownMenuItem
                 className="cursor-pointer py-2"
-                onClick={() => { }}
+                onClick={() => setInviteOpen(true)}
               >
                 Invite people to {workspace.name}
               </DropdownMenuItem>
