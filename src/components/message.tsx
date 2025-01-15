7ceef5f42d1d3025c71032013e4728a453a451bc
Abdul Rehman <91366959+rehmanNRY@users.noreply.github.com>
2025-01-15 20:46:22 +0500

    message toolbar

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -4,6 +4,7 @@ import { format, isToday, isYesterday } from "date-fns"
 import Hint from "./hint";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Thumbnail } from "./thumbnail";
+import { Toolbar } from "./toolbar";
 
 const Renderer = dynamic(() => import("@/components/renderer"), { ssr: false });
 
@@ -118,6 +119,17 @@ export const Message = ({
           ) : null}
         </div>
       </div>
+      {!isEditing && (
+        <Toolbar
+          isAuthor={isAuthor}
+          isPending={false}
+          handleEdit={() => setEditingId(id)}
+          handleThread={() => {}}
+          handleDelete={() => {}}
+          handleReaction={() => {}}
+          hideThreadButton={hideThreadButton}
+        />
+      )  }
     </div>
   )
 
