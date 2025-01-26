36ccf4130f5ee623fd37dc4393c489043d5667e7
Abdul Rehman <91366959+rehmanNRY@users.noreply.github.com>
2025-01-27 01:33:29 +0500

    change title and favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client"
 
-import UserButton from "@/features/auth/components/user-button";
 import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
 
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 import { useEffect, useMemo } from "react";
 import { useRouter } from "next/navigation";
+import { Loader } from "lucide-react";
 
 export default function Home() {
   const router = useRouter();
@@ -18,16 +18,16 @@ export default function Home() {
   useEffect(() => {
     if (isLoading) return;
 
-    if(workspaceId){
+    if (workspaceId) {
       router.replace(`/workspace/${workspaceId}`);
-    } else if(!open){
+    } else if (!open) {
       setOpen(true);
     }
   }, [workspaceId, isLoading, open, setOpen, router])
-  
+
   return (
-    <div className="h-full w-full">
-      <UserButton />
+    <div className="flex h-full items-center justify-center">
+      <Loader className="size-5 animate-spin text-muted-foreground" />
     </div>
   );
 }
