060c3443e853e885c1a585140cf8389b4322ef8c
Abdul Rehman <91366959+rehmanNRY@users.noreply.github.com>
2025-01-26 13:28:41 +0500

    member update & remove feature

--- src/features/members/components/profile.tsx
+++ src/features/members/components/profile.tsx
@@ -1,10 +1,34 @@
 import { Button } from "@/components/ui/button";
 import { Id } from "../../../../convex/_generated/dataModel";
 import { useGetMember } from "../api/use-get-member";
-import { AlertTriangle, Loader, MailIcon, XIcon } from "lucide-react";
+import { AlertTriangle, ChevronDownIcon, Loader, MailIcon, XIcon } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Separator } from "@/components/ui/separator";
 import Link from "next/link";
+import { useUpdateMember } from "../api/use-update-member";
+import { useRemoveMember } from "../api/use-remove-member";
+import { useCurrentMember } from "../api/use-current-member";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { toast } from "sonner";
+import { useConfirm } from "@/hooks/use-confirm";
+import { useRouter } from "next/navigation";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 
 interface ProfileProps {
@@ -17,9 +41,83 @@ const Profile = ({
   memberId,
   onClose,
 }: ProfileProps) => {
-  const { data: member, isLoading: isMemberLoading } = useGetMember({ id: memberId });
+  const router = useRouter();
 
-  if (isMemberLoading) {
+  const workspaceId = useWorkspaceId();
+
+  const [LeaveDialog, confirmLeave] = useConfirm(
+    "Leave workspace",
+    "Are you sure you want to leave the is workspace?"
+  );
+
+  const [RemoveDialog, confirmRemove] = useConfirm(
+    "Remove member",
+    "Are you sure you want to remove this member?"
+  )
+
+  const [UpdateDialog, confirmUpdate] = useConfirm(
+    "Change role",
+    "Are you sure to change this member's role?"
+  )
+
+  const { data: member, isLoading: isMemberLoading } = useGetMember({ id: memberId });
+  const { data: currentMember, isLoading: isLoadingCurrentMember } = useCurrentMember({
+    workspaceId
+  });
+
+  const { mutate: updateMember, isPending: isUpdatingMember } = useUpdateMember();
+  const { mutate: removeMember, isPending: isRemovingMember } = useRemoveMember();
+
+  const onRemove = async () => {
+    const ok = await confirmRemove();
+
+    if(!ok) return;
+
+    removeMember({ id: memberId }, {
+      onSuccess: () => {
+        toast.success("Member removed successfully!");
+        onClose(); // Close the profile after successful removal
+      },
+      onError: (error) => {
+        toast.error(`Failed to remove member`);
+      },
+    });
+  };
+
+  const onLeave = async () => {
+    const ok = await confirmLeave();
+
+    if(!ok) return;
+
+    removeMember({ id: memberId }, {
+      onSuccess: () => {
+        router.replace("/");
+        toast.success("You left the workspace");
+        onClose(); // Close the profile after successful removal
+      },
+      onError: (error) => {
+        toast.error(`Failed to leave the workspace`);
+      },
+    });
+  };
+
+  const onUpdate = async (role: "admin" | "member") => {
+    const ok = await confirmUpdate();
+  
+    if(!ok) return;
+
+    updateMember({ id: memberId, role }, {
+      onSuccess: () => {
+        toast.success("Role changed");
+        onClose(); // Close the profile after successful removal
+      },
+      onError: (error) => {
+        toast.error(`Failed to change role`);
+      },
+    });
+  };
+
+  if (isMemberLoading || isLoadingCurrentMember) {
     return (
       <div className="h-full flex flex-col">
         <div className="flex justify-between items-center h-[49px] px-4 border-b">
@@ -55,6 +153,10 @@ const Profile = ({
   const avatarFallback = member.user.name?.[0] ?? "M";
 
   return (
+    <>
+    <RemoveDialog />
+    <LeaveDialog />
+    <UpdateDialog />
     <div className="h-full flex flex-col">
       <div className="flex justify-between items-center h-[49px] px-4 border-b">
         <p className="text-lg font-bold" >Profile</p>
@@ -72,6 +174,41 @@ const Profile = ({
       </div>
       <div className="flex flex-col p-4">
         <p className="text-xl font-bold" >{member.user.name}</p>
+        {currentMember?.role === "admin" &&
+          currentMember?._id !== memberId ? (
+          <div className="flex items-center gap-2 mt-4">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant={"outline"} className="w-full capitalize">
+                  {member.role} <ChevronDownIcon className="size-4 ml-2" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-full" >
+                <DropdownMenuRadioGroup
+                  value={member.role}
+                  onValueChange={(role) => onUpdate(role as "admin" | "member")}
+                >
+                  <DropdownMenuRadioItem value="admin">
+                    Admin
+                  </DropdownMenuRadioItem>
+                  <DropdownMenuRadioItem value="member">
+                    Member
+                  </DropdownMenuRadioItem>
+                </DropdownMenuRadioGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <Button variant={"outline"} className="w-full" onClick={onRemove} >
+              Remove
+            </Button>
+          </div>
+        ) : currentMember?._id === memberId &&
+          currentMember?.role !== "admin" ? (
+          <div className="mt-4">
+            <Button variant="outline" onClick={onLeave} className="w-full" >
+              Leave
+            </Button>
+          </div>
+        ) : null}
       </div>
       <Separator />
       <div className="flex flex-col p-4" >
@@ -94,6 +231,7 @@ const Profile = ({
         </div>
       </div>
     </div>
+    </>
   )
 }
 
